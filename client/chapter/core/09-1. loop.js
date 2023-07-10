a5a98df8cd5f8571857dd6cf4568e969a2559c65
wlstmd1004v <wlstmd1004v@gmail.com>
2023-07-10 11:42:25 +0900

    5일차 수정

--- client/chapter/core/09-1. loop.js
+++ client/chapter/core/09-1. loop.js
@@ -4,6 +4,7 @@
 
 
 
+
 let repeat = 0;
 while(repeat < 10){
 
@@ -19,56 +20,68 @@ while(repeat < 10){
 
 
 
+
 const frontEndDev = [
-    'HTML',
-    'CSS',
-    'SVG',
-    'JavaScript',
-    'jQuery',
-    'React',
-    'Redux',
+  'HTML',
+  'CSS',
+  'SVG',
+  'JavaScript',
+  'jQuery',
+  'React',
+  'Redux',
 ];
 
-  /* 프론트엔드 개발 집합 항목 출력 ---------------------------------------------- */
+/* 프론트엔드 개발 집합 항목 출력 ---------------------------------------------- */
 
-//  console.log(frontEndDev[0]);
-//  console.log(frontEndDev[1]);
-//  console.log(frontEndDev[2]);
-//  console.log(frontEndDev[3]);
-//  console.log(frontEndDev[4]);
+// console.log(frontEndDev[0]);
+// console.log(frontEndDev[1]);
+// console.log(frontEndDev[2]);
+// console.log(frontEndDev[3]);
+// console.log(frontEndDev[4]);
 // console.log(frontEndDev[5]);
-//  console.log(frontEndDev[6]);
+// console.log(frontEndDev[6]);
 
 
-  /* 프론트엔드 개발 집합을 순환해서 각 아이템을 Console 패널에 출력 -------------------- */
+/* 프론트엔드 개발 집합을 순환해서 각 아이템을 Console 패널에 출력 -------------------- */
 
-  // while 문 (순환 : 순방향)
+// while 문 (순환 : 순방향)
+
+
 
 let z1 = performance.now();
 
 let i = 0;
-
-while (i < frontEndDev.length) {
-    console.log(frontEndDev[i]);
-    i++;
+while ( i < frontEndDev.length) {
+  let value = frontEndDev[i];
+  // console.log(value);
+  i++;
 }
 
 let z2 = performance.now();
 
 console.log(z2 - z1);
 
-  // while 문 (역순환 : 역방향)
 
 
-let l = frontEndDev.length - 1;
 
-while (l >= 0) {
-    console.log(frontEndDev[l]);
-    l--;
+// while 문 (역순환 : 역방향)
+
+
+let l = frontEndDev.length;
+
+while(l >= 0){
+  // --l;
+  // let value = frontEndDev[l];
+  // console.log(frontEndDev[]);
+
+  l--
 }
 
 
-  // 성능 진단 : 순환 vs. 역순환
+
+
+
+
 
 // 원본 파괴
 // 배열의 깊은 복사 
@@ -80,3 +93,10 @@ while(copyArray.length){
   
   console.log( copyArray.shift() );
 }
+
+
+
+
+
+
+// 성능 진단 : 순환 vs. 역순환
