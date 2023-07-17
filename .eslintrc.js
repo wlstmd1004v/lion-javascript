9e0109e958adb12d3a437e9b90592b81bf662be8
wlstmd1004v <wlstmd1004v@gmail.com>
2023-07-17 21:51:13 +0900

    10일차 수업

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    extends: 'eslint:recommended',
+    globals:{
+        getNode:'readOnly',
+    },
+    overrides: [
+    {
+        env: {
+            node: true,
+        },
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+        sourceType: 'script',
+        },
+        },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     },
-    "rules": {
-        "no-unused-vars" : "warn"
-    }
-}
+    rules: {
+        'no-unused-vars': 'warn',
+    },
+};
