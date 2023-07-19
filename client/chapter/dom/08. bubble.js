684005345194546df399e35499a8acb66ed422a1
wlstmd1004v <wlstmd1004v@gmail.com>
2023-07-19 21:55:41 +0900

    12일차 수업

---
+++ client/chapter/dom/08. bubble.js
@@ -0,0 +1,39 @@
+/* ---------------------------- */
+/* Event bubbling & capturing   */
+/* ---------------------------- */
+
+
+/* 버블링 ----------------------------------------------------------------- */
+
+
+const section = getNode('section');
+const article = getNode('article');
+const p = getNode('p');
+
+
+section.addEventListener('click',()=>{
+  console.log('%c section','color:orange');
+})
+
+article.addEventListener('click',(e)=>{
+  // e.stopPropagation()
+  console.log('%c article','color:dodgerblue');
+})
+
+p.addEventListener('click',(e)=>{
+  // e.stopPropagation()
+  console.log('%c p','color:hotpink');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/* 캡처링 ----------------------------------------------------------------- */
