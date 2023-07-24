06f52ff6b329100efd1be84c92a7e07bdb6d18b2
wlstmd1004v <wlstmd1004v@gmail.com>
2023-07-24 16:11:38 +0900

    14일차 수업

---
+++ client/lib/animation/shake.js
@@ -0,0 +1,11 @@
+// import { gsap } from "../../../node_modules/gsap/index.js";
+//     /* global gsap */
+
+//     export const shake = gsap.to('form',{
+//       duration:0.1,
+//       x:-10,
+//       repeat:5,
+//       yoyo:true,
+//       clearProp:'x',
+//       paused:true
+//     })
