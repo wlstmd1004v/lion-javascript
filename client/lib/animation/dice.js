d1192b0d8daa4f9d25e9bfeb8802e71b687dbec4
wlstmd1004v <wlstmd1004v@gmail.com>
2023-07-25 09:19:12 +0900

    15일차 수업

--- client/lib/animation/dice.js
+++ client/lib/animation/dice.js
@@ -1,13 +1,18 @@
 import { attr } from "../dom/attr.js";
 import { getNode } from '../dom/getNode.js'
+import { memo } from "../utils/memo.js";
 // import { gsap } from "../../../node_modules/gsap/index.js";
 
 /* global gsap */
 
 const cube = getNode('#cube');
 
+memo('cube',()=>getNode('#cube'));
+
+
+
 let random;
-gsap.to(cube,{duration:1,rotationX:100,rotationY:-100,ease:'back(10)'});
+gsap.to(memo('cube'),{duration:1,rotationX:100,rotationY:-100,ease:'back(10)'});
 
 
 export function diceAnimation (){
@@ -16,7 +21,7 @@ export function diceAnimation (){
   random = gsap.utils.random([0,1,2,3,4,5]);
 
   function complete(){
-		attr(cube,'dice',random + 1)
+		attr(memo('cube'),'dice',random + 1)
   }
 
   const rotationValue = [
